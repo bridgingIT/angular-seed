b0b3febcd626ae52f8d437fde7526c74df8cacff
Christian Janz <christian.janz@bridging-it.de>
2014-10-25 16:06:30 +0200

    asldk

--- app/components/version/interpolate-filter.js
+++ app/components/version/interpolate-filter.js
@@ -4,6 +4,7 @@ angular.module('myApp.version.interpolate-filter', [])
 
 .filter('interpolate', ['version', function(version) {
   return function(text) {
-    return String(text).replace(/\%VERSION\%/mg, version);
+    return text;
+    //return String(text).replace(/\%VERSION\%/mg, version);
   };
 }]);
