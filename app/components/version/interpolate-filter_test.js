12fb006d458c2f758cdd1be3714268278dddd26d
Christian Janz <christian.janz@gmail.com>
2014-10-27 12:21:19 +0100

    Add another test

--- app/components/version/interpolate-filter_test.js
+++ app/components/version/interpolate-filter_test.js
@@ -1,15 +1,27 @@
 'use strict';
 
-describe('myApp.version module', function() {
-  beforeEach(module('myApp.version'));
+describe('myApp.version module', function () {
+    beforeEach(module('myApp.version'));
 
-  describe('interpolate filter', function() {
-    beforeEach(module(function($provide) {
-      $provide.value('version', 'TEST_VER');
-    }));
+    describe('interpolate filter', function () {
 
-    it('should replace VERSION', inject(function(interpolateFilter) {
-      expect(interpolateFilter('before %VERSION% after')).toEqual('before TEST_VER after');
-    }));
-  });
+        var filter;
+
+        beforeEach(module(function ($provide) {
+            $provide.value('version', 'TEST_VER');
+        }));
+
+        beforeEach(inject(function (interpolateFilter) {
+            filter = interpolateFilter;
+        }));
+
+        it('should replace VERSION', function () {
+            expect(filter('before %VERSION% after')).toEqual('before TEST_VER after');
+        });
+
+        it('should leave other text untouched', function () {
+                expect(filter('other text without VERSION')).toEqual('other text without VERSION');
+            }
+        );
+    });
 });
